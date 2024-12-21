bbfe0e0a60b7d6c32d4685b67305de1c5e47e793
UltraQbik <no1skill@yandex.ru>
2024-12-21 04:01:02 +0300

    Add clamping function

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ import pyaudio
 import numpy as np
 
 
+def clamp(x: float, min_: float = -1, max_: float = 1) -> float:
+    """
+    Clamps the value to be in range of -1 to 1
+    :param x: value
+    :param min_: minimum allowed
+    :param max_: maximum allowed
+    :return: clamped value
+    """
+
+    return max(min_, min(max_, x))
+
+
 class Application:
     """
     Main application class
     """
 
-    def __init__(self, sample_rate: int, byte_width: int):
+    def __init__(self, sample_rate: int):
         self.sample_rate: int = sample_rate
-        self.byte_width: int = byte_width
+        self.byte_width: int = 4
 
         self.pyaudio: pyaudio.PyAudio = pyaudio.PyAudio()
         self.stream = self.pyaudio.open(
@@ -27,9 +39,7 @@ class Application:
 
 
 def main():
-    app = Application(
-        sample_rate=48000,
-        byte_width=4)
+    app = Application(sample_rate=48000)
     app.run()
 
 
